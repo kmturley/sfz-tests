11730588cff4b77d3cab8d9fd7eff035bb6ccce8
Kim T <kimturley@gmail.com>
2023-07-20 15:50:11 -0700

    Add xml versions from sfizz_preprocessor

---
+++ sfz-to-xml.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: File
+# ./sfz-to-xml.sh "./sfz1 basic tests/01 - Amp LFO/01 - amp lfo freq.sfz"
+#
+# Usage: Folder
+# ./sfz-to-xml.sh .
+
+
+if [ ! -f sfizz_preprocessor ]
+then
+  curl -LO https://github.com/studiorack/sfizz/releases/download/v1.1.1/sfizz-preprocessor-mac.zip
+  unzip sfizz-preprocessor-mac.zip
+  rm sfizz-preprocessor-mac.zip
+fi
+
+IFS=$'\n'
+
+if [[ $1 == *.sfz ]]
+then
+  # Convert file.
+  ./sfizz_preprocessor "$1" --mode=xml > "${1%.*}.xml"
+else
+  # Convert folder.
+  for file in $(find "$1" -type f -name '*.sfz')
+    do ./sfizz_preprocessor "$file" --mode=xml > "${file%.*}.xml"
+  done
+fi
