365d1c7f676ab77ee8df63ce9c275ca6cbd56585
Kim T <kimturley@gmail.com>
2024-01-10 22:30:04 -0800

    Yaml version of files

---
+++ sfz-to-yaml.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Requires `npm install -g js-yaml`
+#
+# Usage: File
+# ./sfz-to-yaml.sh "./sfz1 basic tests/01 - Amp LFO/01 - amp lfo freq.json"
+#
+# Usage: Folder
+# ./sfz-to-yaml.sh .
+
+if [[ $1 == *.json ]]
+then
+  # Convert file.
+  js-yaml "$1" > "${1%.*}.yaml"
+else
+  # Convert folder.
+  find "$1" -type f -name "*.json" | while read file
+    do js-yaml "$file" > "${file%.*}.yaml"
+  done
+fi
